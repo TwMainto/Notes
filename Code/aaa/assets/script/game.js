103de28677d384c2a9c0188a7ad975278be74591
hao <3047927586@qq.com>
2019-11-18 18:06:46 +0800

    2019.11.18 :
    添加笔记 - JS 记事本
    添加代码 - aaa

--- Code/aaa/assets/script/game.js
+++ Code/aaa/assets/script/game.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 cc.Class({
     extends: cc.Component,
 
@@ -12,3 +13,20 @@ cc.Class({
         
     },
 });
+||||||| merged common ancestors
+=======
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+       boom: cc.Node,
+       
+    },
+
+    start () {
+        //boomAction = this.boom.getComponent(cc.Animation);
+        //boomAction.play();
+        
+    },
+});
+>>>>>>> 2019.11.18 :
